8177a3a10f46f9a39396fd258a25051476b7e659
anmol <anmoldogra676@gmail.com>
2021-09-17 10:00:11 +0530

    added delete Task, add Task

---
+++ React_ToDo/todo/src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+import Todo from './components/todo';
+
+
+function App() {
+  return (
+   <Todo/>
+  );
+}
+
+export default App;
---
+++ React_ToDo/todo/src/components/todo.jsx
@@ -0,0 +1,77 @@
+// react snippets
+// rcc
+// Todo --> 2 components
+// Input container , TaskList
+// parent se child ko data send krna ho toh --> props se send kr sko 
+// jo state children m use aayegi use children m add kr do or jo dono m use aayegi use parent m rkho 
+// redux--> state manageent se 
+
+
+
+import React, { Component } from 'react';
+//1.render -> static ui define
+// /2. identify different variables that 
+// can change throughout the life time -> state
+// 3. rewrite render using those state variables
+// 4. event listener to change the state
+export default class Todo extends Component {
+    state = {
+        taskList: ["Task-1", "Task-2", "Task-3", "Task4"],
+        currTask: ""
+    }
+    deleteTask = (cTask) => {
+        // current - rest of the task 
+        let filteredtasks = this.state.taskList
+            .filter(function (task) {
+                return task !== cTask;
+            })
+        this.setState({
+            taskList: filteredtasks
+        });
+    }
+    handleCurrTask = (e) => {
+        let currValue = e.target.value;
+        this.setState({
+            currTask: currValue
+        })
+
+    }
+    addTask = () => {
+        let currTask = this.state.currTask;
+        // let tempArr = this.state.taskList;
+        // for (let i = 0; i < this.state.taskList.length; i++) {
+        //     tempArr.push(this.state.taskList[i]);
+        // }
+        // tempArr.push(currTask);
+        let tempArr = [...this.state.taskList, currTask]
+        this.setState({
+            taskList: tempArr,
+            currTask:""
+        })
+    }
+    render() {
+        return (
+            <div>
+                <div className="input-container">
+                    <input type="text" value={this.state.currTask}
+                    onChange={this.handleCurrTask} />
+                    <button onClick={this.addTask}>submit</button>
+                </div>
+                <div className="task-list">
+                    <ul>
+                        {this.state.taskList.map((task) => {
+                            return (
+                                <li className="tasklist">
+                                    <p>{task}</p>
+                                    <button onClick={() => 
+                                        { this.deleteTask(task) }}>
+                                        Delete</button>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
